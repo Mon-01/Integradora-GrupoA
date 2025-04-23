166606f8b4c156ef7d42ad85bd45793efbe6cc56
Ignacio Lozano <Ignacio.lozano2@educa.madrid.org>
2025-04-23 13:11:18 +0200

    Archivo que guarda los loggs de la aplicacion añadido correctamente

--- AplicacionAdministracion/src/main/java/com/example/aplicacionadministracion/controller/controladorMVCAdmin.java
+++ AplicacionAdministracion/src/main/java/com/example/aplicacionadministracion/controller/controladorMVCAdmin.java
@@ -1,12 +1,17 @@
 package com.example.aplicacionadministracion.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.slf4j.Logger;
+
 @Controller
 public class controladorMVCAdmin {
-    @GetMapping("/paso1")
+    private static final Logger logger = LoggerFactory.getLogger(controladorMVCAdmin.class);
+    @GetMapping("/Paso1")
     public String p1(){
+
         return "vista1";
     }
 
