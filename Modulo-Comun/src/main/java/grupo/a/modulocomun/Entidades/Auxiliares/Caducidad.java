2250570952292f96ba1372c19fdadee809f8bbdd
LaRoca33 <arocaangel19@gmail.com>
2025-04-25 10:41:07 +0200

    commit de seguridad

--- Modulo-Comun/src/main/java/grupo/a/modulocomun/Entidades/Auxiliares/Caducidad.java
+++ Modulo-Comun/src/main/java/grupo/a/modulocomun/Entidades/Auxiliares/Caducidad.java
@@ -1,6 +1,6 @@
 package grupo.a.modulocomun.Entidades.Auxiliares;
 
-import jakarta.persistence.Embeddable;
+/*
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,3 +12,5 @@ public class Caducidad {
     private int anio;
     private int mes;
 }
+
+ */
