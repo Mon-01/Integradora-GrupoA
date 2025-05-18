b7c61659682dc60ed00974d6ef9f09366e8410c5
Mon-01 <esparamonica@gmail.com>
2025-05-18 22:52:17 +0200

    eliminación de productos completamente funcional

--- AplicacionAdministracion/src/main/resources/static/js/EliminarVariosProductos.js
+++ AplicacionAdministracion/src/main/resources/static/js/EliminarVariosProductos.js
@@ -1,25 +1,32 @@
-//para eliminar productos con el botón
+//con ready nos esperamos a que cargue el dom
 $(document).ready(function() {
-    $('.eliminar-btn').on('click', function () {
+    //cogemos los resultados de la busqueda y localizamos el elemento por la clase
+    $('#resultados').on('click', '.eliminar-btn', function() {
+        console.log("this:", this);
+        console.log("$(this):", $(this));
+        console.log("event.target:", event.target);
+        //le pasamos el id y el tr más cercano para borrarlo
         const id = $(this).data('id');
         const row = $(this).closest('tr');
 
-        if (confirm('¿Está seguro de que desea eliminar este producto? Esta acción no se puede deshacer.')) {
+        console.log("ID a eliminar:", id); // <--- AÑADE ESTA LÍNEA
+
+        if(confirm('¿Está seguro de que desea eliminar este producto? Esta acción no se puede deshacer.')) {
             $.ajax({
                 url: '/api/admin/del/producto/' + id,
                 type: 'DELETE',
-                success: function (result) {
-                    row.fadeOut(400, function () {
+                success: function(result) {
+                    row.fadeOut(400, function() {
                         row.remove();
                     });
                     alert('Producto eliminado correctamente');
                 },
-                error: function (xhr) {
+                error: function(xhr) {
                     alert('Error al eliminar el producto: ' + xhr.responseText);
                 }
             });
         }
-    })
+    });
 });
 
 
